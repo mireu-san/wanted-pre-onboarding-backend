e71829575a0445c227f5699a349fcd35c9a17ce4
Chesed Kim <starmireu@gmail.com>
2023-10-05 21:47:23 +0900

    1차 작업 DRAFT

--- CompanyApp/models.py
+++ CompanyApp/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 
 class Company(models.Model):
-    name = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-    region = models.CharField(max_length=255)
+    """
+    기본 세부 정보가 있는 회사를 대표하는 모델입니다.
+    """
+
+    name = models.CharField(
+        max_length=100, unique=True
+    )  # Considering the name to be unique
+    description = models.TextField()
+    established_at = models.DateField()  # If time info is not required
+    founder = models.CharField(max_length=255)  # Allowing for longer names
 
     class Meta:
-        app_label = "CompanyApp"
+        db_table = "companies"  # Explicitly defining the table name
+        ordering = ["established_at"]  # Default ordering by establishment date
 
-    def __str__(self):
-        return self.name
+    def get_establishment_duration(self):
+        """
+        회사가 설립된 연도를 return 합니다.
+        """
+        return (timezone.now().date() - self.established_at).days // 365
---
+++ README.md
@@ -0,0 +1,9 @@
+개요.
+WANTED 의 백엔드 인턴 사전 과제인 '채용 서비스' 를 ERD, RDBMS로 개발중인 간이 프로젝트 입니다.
+
+사용 기술.
+Django, PostgreSQL, DRF, flake8, black formatter
+
+Limitation.
+- Docker 컨테이너화 진행을 생략.
+- 토큰 설정 생략.
