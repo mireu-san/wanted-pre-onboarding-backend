5864f2be8b7ff5f671ff641a78f5a7c6d567b673
Chesed Kim <starmireu@gmail.com>
2023-10-06 16:28:11 +0900

    fix - SubmitHistoryApp : CompanyApp, JobPostingApp models to TDD

--- SubmitHistoryApp/models.py
+++ SubmitHistoryApp/models.py
@@ -4,10 +4,16 @@ from JobPostingApp.models import JobPosting
 
 
 class SubmitHistory(models.Model):
-    user_id = models.IntegerField()
-    job_posting_id = models.IntegerField()
-    status = models.CharField(max_length=255)
+    STATUS_CHOICES = [
+        ("applied", "Applied"),
+        ("rejected", "Rejected"),
+        ("accepted", "Accepted"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    job_posting = models.ForeignKey(JobPosting, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES)
     apply_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = [["user_id", "job_posting_id"]]
+        unique_together = [["user", "job_posting"]]
--- SubmitHistoryApp/tests.py
+++ SubmitHistoryApp/tests.py
@@ -1,25 +1,44 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from SubmitHistoryApp.models import SubmitHistory
-from JobPostingApp.models import JobPosting
+from JobPostingApp.models import JobPosting, TechStack, Company
+from UserApp.models import User
 
 
 class SubmitHistoryAPITest(TestCase):
     def setUp(self):
+        self.client = APIClient()
+
+        # User 객체 생성
+        self.user = User.objects.create_user(
+            email="test@example.com", username="testuser", password="testpass"
+        )
+
+        # Company 객체 생성
+        self.company = Company.objects.create(
+            name="Example Company", description="This is an example company."
+        )
+
+        # TechStack 객체 생성
+        tech1 = TechStack.objects.create(name="Python")
+        tech2 = TechStack.objects.create(name="Django")
+
+        # JobPosting 객체 생성
         self.job_posting = JobPosting.objects.create(
-            company_id=1,
+            company=self.company,  # 생성된 Company 객체를 사용
             position="Backend Developer",
             reward=60000,
             description="A job description...",
-            tech_stack="Python, Django",
         )
+        # JobPosting의 tech_stack 필드에 TechStack 객체 추가
+        self.job_posting.tech_stack.add(tech1, tech2)
 
     def test_create_submit_history(self):
         response = self.client.post(
             "/submit_history/submit_histories/",
             {
-                "user_id": 1,
-                "job_posting_id": self.job_posting.id,
+                "user": self.user.id,
+                "job_posting": self.job_posting.id,
                 "status": "applied",
             },
         )
@@ -28,17 +47,15 @@ class SubmitHistoryAPITest(TestCase):
             response.data,
             {
                 "id": 1,
-                "user_id": 1,
-                "job_posting_id": self.job_posting.id,
+                "user": self.user.id,
+                "job_posting": self.job_posting.id,
                 "status": "applied",
-                "apply_date": response.data[
-                    "apply_date"
-                ],  # auto_now_add 필드는 클라이언트에서 설정되지 않습니다.
+                "apply_date": response.data["apply_date"],
             },
         )
 
         # Validate the database state.
         submit_history = SubmitHistory.objects.get(id=1)
-        self.assertEqual(submit_history.user_id, 1)
-        self.assertEqual(submit_history.job_posting_id, self.job_posting.id)
+        self.assertEqual(submit_history.user.id, self.user.id)
+        self.assertEqual(submit_history.job_posting.id, self.job_posting.id)
         self.assertEqual(submit_history.status, "applied")
