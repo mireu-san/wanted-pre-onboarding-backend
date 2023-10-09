05109a090a2be752f83da814c73f06b41919aa31
Chesed Kim <starmireu@gmail.com>
2023-10-09 10:36:05 +0900

    test : CompanyApp - refactored, reverse url

--- CompanyApp/tests.py
+++ CompanyApp/tests.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.test import TestCase
 from rest_framework.test import APIClient
 from CompanyApp.models import Company
@@ -8,114 +9,86 @@ class CompanyAPITest(TestCase):
     def setUp(self):
         """테스트를 위한 사전 설정"""
         self.client = APIClient()
-        self.company = Company.objects.create(
-            name="Paper Company",
-            country="KR",
-            region="Seoul",
-            description="Sample description",
-            established_at="2023-10-06",
-            founder="Mr Kim",
-        )
+        self.company_data = {
+            "name": "Paper Company",
+            "country": "KR",
+            "region": "Seoul",
+            "description": "Sample description",
+            "established_at": "2023-10-06",
+            "founder": "Mr Kim",
+        }
+        self.company = Company.objects.create(**self.company_data)
+        self.company_url = reverse("company-detail", kwargs={"pk": self.company.id})
 
     def test_retrieve_company(self):
         """특정 회사의 정보를 가져오는 API 테스트"""
-        response = self.client.get(f"/company/companies/{self.company.id}/")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data["name"], "Paper Company")
+        response = self.client.get(self.company_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], self.company_data["name"])
 
     def test_create_company(self):
         """회사를 새로 생성하는 API 테스트"""
-        response = self.client.post(
-            "/company/companies/",
-            {
-                "name": "Black Company",
-                "country": "Snezhnaya",
-                "region": "Seoul",
-                "description": "Sample description for Paper Company",
-                "established_at": "2020-01-01",
-                "founder": "Queen",
-            },
-        )
-        print(response.data)
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data["name"], "Black Company")
+        new_company_data = {
+            "name": "Black Company",
+            "country": "Snezhnaya",
+            "region": "Seoul",
+            "description": "Sample description for Paper Company",
+            "established_at": "2020-01-01",
+            "founder": "Queen",
+        }
+        response = self.client.post(reverse("company-list"), new_company_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], new_company_data["name"])
 
     def test_update_company(self):
         """회사 정보를 업데이트하는 API 테스트"""
-        response = self.client.put(
-            f"/company/companies/{self.company.id}/",
-            {
-                "name": "Updated Co.",
-                "country": "US",
-                "region": "NY",
-                "description": "Updated description",
-                "established_at": "2019-01-01",
-                "founder": "Jane Smith",
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        # 수정된 정보를 확인합니다.
-        self.company.refresh_from_db()  # 데이터베이스로부터 수정된 정보를 가져옵니다.
-        self.assertEqual(self.company.name, "Updated Co.")
-        self.assertEqual(self.company.country, "US")
-        self.assertEqual(self.company.region, "NY")
+        updated_data = {
+            "name": "Updated Co.",
+            "country": "US",
+            "region": "NY",
+            "description": "Updated description",
+            "established_at": "2019-01-01",
+            "founder": "Aiden Pearce",
+        }
+        response = self.client.put(self.company_url, updated_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.company.refresh_from_db()
+        self.assertEqual(self.company.name, updated_data["name"])
+        self.assertEqual(self.company.country, updated_data["country"])
 
     def test_delete_company(self):
         """회사 정보를 삭제하는 API 테스트"""
-        response = self.client.delete(f"/company/companies/{self.company.id}/")
-        self.assertEqual(response.status_code, 204)  # 204: No Content (성공적으로 삭제됨)
-        # 실제로 회사 정보가 삭제되었는지 확인합니다.
+        response = self.client.delete(self.company_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         with self.assertRaises(Company.DoesNotExist):
             Company.objects.get(id=self.company.id)
 
     def test_create_company_with_missing_name(self):
         """이름 필드 없이 회사 생성 API 호출 시 오류가 반환되는지 테스트"""
-        response = self.client.post(
-            "/company/companies/",
-            {
-                "country": "Snezhnaya",
-                "region": "Seoul",
-                "description": "Sample description for Paper Company",
-                "established_at": "2020-01-01",
-                "founder": "Queen",
-            },
-        )
+        incomplete_data = {
+            "country": "Snezhnaya",
+            "region": "Seoul",
+            "description": "Sample description for Paper Company",
+            "established_at": "2020-01-01",
+            "founder": "Queen",
+        }
+        response = self.client.post(reverse("company-list"), incomplete_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_company_with_duplicate_name(self):
         """중복된 이름으로 회사 생성 API 호출 시 오류가 반환되는지 테스트"""
-        Company.objects.create(
-            name="Duplicate Company",
-            country="KR",
-            region="Seoul",
-            description="Sample",
-            established_at="2023-10-06",
-            founder="Mr Kim",
-        )
-        response = self.client.post(
-            "/company/companies/",
-            {
-                "name": "Duplicate Company",
-                "country": "US",
-                "region": "NY",
-                "description": "Another description",
-                "established_at": "2020-01-01",
-                "founder": "Jane Smith",
-            },
-        )
+        response = self.client.post(reverse("company-list"), self.company_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_company_with_invalid_date(self):
         """유효하지 않은 날짜 형식으로 회사 생성 API 호출 시 오류가 반환되는지 테스트"""
-        response = self.client.post(
-            "/company/companies/",
-            {
-                "name": "Test Company",
-                "country": "Snezhnaya",
-                "region": "Seoul",
-                "description": "Sample description for Paper Company",
-                "established_at": "invalid_date",
-                "founder": "Queen",
-            },
-        )
+        invalid_data = {
+            "name": "Test Company",
+            "country": "Snezhnaya",
+            "region": "Seoul",
+            "description": "Sample description for Paper Company",
+            "established_at": "invalid_date",
+            "founder": "Queen",
+        }
+        response = self.client.post(reverse("company-list"), invalid_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
